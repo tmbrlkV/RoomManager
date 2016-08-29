fb6458eb40ae39ab49864e545c66114beb2edee2
Anton Valov <anton.valov.o@gmail.com>
2016-08-29 14:08:42 +0300

    Added total remove user from rooms

--- src/main/java/com/manager/command/Command.java
+++ src/main/java/com/manager/command/Command.java
@@ -8,10 +8,11 @@ public interface Command {
 
     long execute(User user, long roomId);
 
-    String CREATE_ROOM = "createLobby";
+    String CREATE_ROOM = "createRoom";
     String REMOVE_ROOM = "removeRoom";
     String ADD_USER_TO_ROOM = "addUserToRoom";
     String REMOVE_USER_FROM_ROOM = "removeUserFromRoom";
+    String REMOVE_USER_FROM_ALL_ROOMS = "removeUserFromAllRooms";
     String TO_USER = "toUser";
     String DEFAULT = "";
 }
--- src/main/java/com/manager/service/RoomManager.java
+++ src/main/java/com/manager/service/RoomManager.java
@@ -15,26 +15,33 @@ public class RoomManager {
     private static final Logger logger = LoggerFactory.getLogger(RoomManager.class);
 
     private List<Room> rooms = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
+    private static Room lobby;
 
-    public Room createRoom() {
-        Room room = new Room(Instant.now().getEpochSecond());
+    public Room createRoom(User user) {
+        Instant now = Instant.now();
+        Room room = new Room(now.getEpochSecond() + now.getNano());
         rooms.add(room);
+        users.add(user);
         logger.debug("Rooms size: {}.", rooms.size());
         return room;
     }
 
     public Room createLobby(long roomId) {
-        if (roomId <= 0L) {
-            return createRoom();
+        if (lobby == null) {
+            lobby = new Room(roomId);
+            rooms.add(lobby);
+            logger.debug("Rooms size: {}.", rooms.size());
         }
-        Room room = new Room(roomId);
-        rooms.add(room);
-        logger.debug("Rooms size: {}.", rooms.size());
-        return room;
+        return lobby;
     }
 
     public List<Room> getRooms() {
         return Collections.unmodifiableList(rooms);
+    }
+
+    public List<User> getUsers() {
+        return Collections.unmodifiableList(users);
     }
 
     public Room removeRoom(long roomId) {
@@ -45,7 +52,12 @@ public class RoomManager {
 
     public Room addUserToRoom(User user, long roomId) {
         Optional<Room> foundRoom = findRoom(roomId);
-        foundRoom.map(room -> room.addUser(user)).orElse(false);
+        foundRoom.map(room -> {
+            System.err.println(user);
+            room.addUser(user);
+            users.add(user);
+            return true;
+        }).orElse(false);
         logger.debug("Found room: {}", foundRoom);
         return foundRoom.orElse(new Room(-1L));
     }
@@ -53,7 +65,23 @@ public class RoomManager {
     public Room removeUserFromRoom(User user, long roomId) {
         Optional<Room> foundRoom = findRoom(roomId);
         foundRoom.map(room -> room.removeUser(user)).orElse(false);
-        return foundRoom.orElse(new Room(-1L));
+        Room room = foundRoom.orElse(new Room(-1L));
+//        if (room.getUsers().isEmpty()) {
+//            room = removeRoom(room.getId());
+//        }
+        return room;
+    }
+
+    public Room removeUserFromAllRooms(User user) {
+        rooms.stream().forEach(room -> {
+            logger.debug("Try to remove from {}", room);
+            if (room.getUsers().contains(user)) {
+                removeUserFromRoom(user, room.getId());
+                logger.debug("User {} was removed from {}", user, room);
+            }
+        });
+
+        return lobby;
     }
 
     private Optional<Room> findRoom(long roomId) {
